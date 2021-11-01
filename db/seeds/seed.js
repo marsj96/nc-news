ef8f58accf96206109a6b00f023cf446b11cbb5c
Jack <jack.oliver.marsden@gmail.com>
2021-11-01 10:33:33 +0000

    finishes setup - ready to start seed.js

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,9 @@
+const db = require('../connection')
+
 const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
   // 1. create tables
+  return db.query("DROP TABLE IF EXISTS topics")
   // 2. insert data
 };
 
