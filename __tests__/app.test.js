c3aebdb1359555c6c388641104454f76cebf96f2
Jack <jack.oliver.marsden@gmail.com>
2021-11-01 12:33:42 +0000

    creates first endpoint - GET for /api/topics and tests for correct output

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,28 @@
 const db = require('../db/connection.js');
+const { expect } = require('@jest/globals');
+const request = require('supertest')
 const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const seed = require('../db/seeds/seed.js');
+const app = require('../app')
 
 beforeEach(() => seed(testData));
+
+describe('APP', () => {
+    describe('/api/topics - GET', () => {
+        it('Status - 200, should respond with an array of objects containing "slug" and "description"', () => {
+            return request(app)
+            .get('/api/topics')
+            .expect(200)
+            .then(({body: {topics}})=>{
+                topics.forEach((topic)=>{
+                    expect(topic).toMatchObject({
+                        slug: expect.any(String),
+                        description: expect.any(String)
+                    })
+                })
+            })
+        });
+    });
+});
+
 afterAll(() => db.end());
--- routes/topics.router.js
+++ routes/topics.router.js
@@ -1,5 +1,7 @@
+const { getTopics } = require('../controllers/news.controller')
+
 const topicsRouter = require('express').Router()
 
-topicsRouter.route('/').get()
+topicsRouter.route('/').get(getTopics)
 
 module.exports = topicsRouter
