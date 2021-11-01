3b14a231cee8b7e5176cb2cfa4bcb1a19ac7af9c
Jack <jack.oliver.marsden@gmail.com>
2021-11-01 11:04:35 +0000

    Drops and creates tables ready for insertion of data

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -3,7 +3,56 @@ const db = require('../connection')
 const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
   // 1. create tables
-  return db.query("DROP TABLE IF EXISTS topics")
+    return db.query("DROP TABLE IF EXISTS topics")
+  .then(()=>{
+    return db.query("DROP TABLE IF EXISTS users")
+  })
+  .then(()=>{
+    return db.query("DROP TABLE IF EXISTS articles")
+  })
+  .then(()=>{
+    return db.query("DROP TABLE IF EXISTS comments")
+  })
+  .then(()=>{
+    return db.query(`
+      CREATE TABLE topics (
+        slug VARCHAR PRIMARY KEY NOT NULL UNIQUE,
+        description VARCHAR(100) NOT NULL
+      )`)
+  })
+  .then(()=>{
+    return db.query(`
+    CREATE TABLE users (
+      username VARCHAR PRIMARY KEY NOT NULL UNIQUE,
+      avatar_URL VARCHAR,
+      name VARCHAR NOT NULL
+    )`)
+  })
+  .then(()=>{
+    return db.query(`
+      CREATE TABLE articles (
+        article_id SERIAL PRIMARY KEY,
+        title VARCHAR NOT NULL,
+        body VARCHAR NOT NULL,
+        votes INT DEFAULT 0,
+        topic VARCHAR REFERENCES topics(slug),
+        author VARCHAR REFERENCES users(username),
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
+    `)
+  })
+  .then(()=>{
+    return db.query(`
+      CREATE TABLE comments (
+        comment_id SERIAL PRIMARY KEY,
+        author VARCHAR REFERENCES users(username),
+        article_id INT REFERENCES articles(article_id),
+        votes INT DEFAULT 0,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        body VARCHAR NOT NULL
+      )
+    `)
+  })
   // 2. insert data
 };
 
