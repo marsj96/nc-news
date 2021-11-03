0247344036dd22c9c6770578a1ed88af0d05388c
Jack <jack.oliver.marsden@gmail.com>
2021-11-03 11:49:38 +0000

    adds util function to check sort_by query

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const db = require('./db/connection')
+
 exports.checkObjectLength = (obj) => {
     let length = 0;
 
@@ -7,4 +9,27 @@ exports.checkObjectLength = (obj) => {
         }
     }
     return length
+}
+
+exports.checksSortBy = (sortByQuery, queryString) => {
+
+    if(sortByQuery === "title") {
+        return db.query(queryString += ` ORDER BY title ASC`)
+    }
+
+    if(sortByQuery === "article_id") {
+        return db.query(queryString += ` ORDER BY article_id ASC`)
+    }
+
+    if(sortByQuery === "topic") {
+        return db.query(queryString += ` ORDER BY topic ASC`)
+    }
+
+    if(sortByQuery === "votes") {
+        return db.query(queryString += ` ORDER BY votes DESC`)
+    }
+
+    if(sortByQuery === "comment_count") {
+        return db.query(queryString += ` ORDER BY comment_count DESC`)
+    }
 }
