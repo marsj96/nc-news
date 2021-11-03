fbaef0f060ab127f542e681d78ca8ea394890930
Jack <jack.oliver.marsden@gmail.com>
2021-11-03 11:27:58 +0000

    adds a promise.reject for when passed a value that isn't a valid column in sort_by

--- models/news.model.js
+++ models/news.model.js
@@ -121,7 +121,7 @@ exports.fetchCommentsByArticleId = (id) => {
     })
 }
 
-exports.fetchArticles = (sort_by) => {
+exports.fetchArticles = (sort_by = "created_at") => {
 
     let articlesQuery = 
     `SELECT articles.*,
@@ -131,6 +131,13 @@ exports.fetchArticles = (sort_by) => {
     ON articles.article_id = comments.article_id 
     GROUP BY articles.article_id`
 
+    console.log(sort_by)
+
+    if(!["title", "article_id", "topic", "votes", "comment_count", "created_at"].includes(sort_by)) {
+        return Promise.reject({status: 400, msg: "Bad request"})
+    }
+
+    //sorts by title ASC
     if(sort_by === "title") {
         return db.query(articlesQuery += ` ORDER BY title ASC`)
         .then(({rows})=>{
@@ -138,6 +145,7 @@ exports.fetchArticles = (sort_by) => {
         })
     }
 
+    //sorts by topic article_id ASC
     if(sort_by === "article_id") {
         return db.query(articlesQuery += ` ORDER BY article_id ASC`)
         .then(({rows})=>{
@@ -145,6 +153,7 @@ exports.fetchArticles = (sort_by) => {
         })
     }
 
+    //sorts by topic ASC
     if(sort_by === "topic") {
         return db.query(articlesQuery += ` ORDER BY topic ASC`)
         .then(({rows})=>{
@@ -152,22 +161,24 @@ exports.fetchArticles = (sort_by) => {
         })
     }
 
+    //sorts by votes DESC, as we want the highest comment_counts first
     if(sort_by === "votes") {
-        return db.query(articlesQuery += ` ORDER BY votes ASC`)
+        return db.query(articlesQuery += ` ORDER BY votes DESC`)
         .then(({rows})=>{
             return rows
         })
     }
 
+    //sort_by comment_count DESC, as we want the highest comment_counts first
     if(sort_by === "comment_count") {
-        return db.query(articlesQuery += ` ORDER BY comment_count ASC`)
+        return db.query(articlesQuery += ` ORDER BY comment_count DESC`)
         .then(({rows})=>{
             return rows
         })
     }
 
     //handles sort_by default to be created_at DESC, so newest posts first unless specified differentl in the query
-    if(!sort_by) {
+    if(sort_by === "created_at") {
         return db.query(articlesQuery += ` ORDER BY created_at DESC`)
         .then(({rows})=>{
             return rows
