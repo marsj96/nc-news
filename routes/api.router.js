0666ac5e1d8adf1a2d6bc81f7ada9f2d97e1d6ff
Jack <jack.oliver.marsden@gmail.com>
2021-11-04 14:45:22 +0000

    adds router for /api

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,9 +1,11 @@
+const { getApi } = require('../controllers/news.controller')
 const articlesRouter = require('./articles.router')
 const commentsRouter = require('./comments.router')
 const topicsRouter = require('./topics.router')
 
 const apiRouter = require('express').Router()
 
+apiRouter.route('/').get(getApi)    
 apiRouter.use('/topics', topicsRouter)
 apiRouter.use('/articles', articlesRouter)
 apiRouter.use('/comments', commentsRouter)
