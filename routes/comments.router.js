a9f0444f0b1430a13ce4218c3391916bee72e0be
Jack <jack.oliver.marsden@gmail.com>
2021-11-04 14:29:29 +0000

    adds route for /api/comments/comment_id

--- routes/comments.router.js
+++ routes/comments.router.js
@@ -1,5 +1,8 @@
+const { deleteComment } = require('../controllers/news.controller')
+
 const commentsRouter = require('express').Router()
 
 commentsRouter.route('/').get()
+commentsRouter.route('/:comment_id').delete(deleteComment)
 
 module.exports = commentsRouter
