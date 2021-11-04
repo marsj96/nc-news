b900b606bd0e817a270680f280c2cdb09b81f473
Jack <jack.oliver.marsden@gmail.com>
2021-11-04 14:29:53 +0000

    creates model for delete request

--- models/news.model.js
+++ models/news.model.js
@@ -214,3 +214,11 @@ exports.postComment = (id, body, username) => {
 
     
 }
+
+exports.removeComment = (id) => {
+
+    return db.query(
+        `DELETE FROM comments
+        WHERE comment_id = $1`, [id])
+        
+}
