10a2fd69f35e918a599c0d91c904fa7dcc930b3a
Jack <jack.oliver.marsden@gmail.com>
2021-11-05 22:18:22 +0000

    added not nulls to relevant places in SQL seed file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -25,7 +25,7 @@ const seed = (data) => {
     return db.query(`
     CREATE TABLE users (
       username VARCHAR PRIMARY KEY NOT NULL,
-      avatar_URL VARCHAR,
+      avatar_URL VARCHAR NOT NULL,
       name VARCHAR NOT NULL
     )`)
   })
@@ -46,8 +46,8 @@ const seed = (data) => {
     return db.query(`
       CREATE TABLE comments (
         comment_id SERIAL PRIMARY KEY,
-        author VARCHAR REFERENCES users(username),
-        article_id INT REFERENCES articles(article_id),
+        author VARCHAR REFERENCES users(username) NOT NULL,
+        article_id INT REFERENCES articles(article_id) ON DELETE CASCADE,
         votes INT DEFAULT 0,
         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         body VARCHAR NOT NULL
