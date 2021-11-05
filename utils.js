7b2233a1d23e84608accd1b354cd557ad73262f2
Jack <jack.oliver.marsden@gmail.com>
2021-11-05 19:51:50 +0000

    updates utils to create a checkDB function. This queries the DB to check if the topic exists

--- utils.js
+++ utils.js
@@ -11,33 +11,54 @@ exports.checkObjectLength = (obj) => {
     return length
 }
 
-exports.checksSortBy = (sortByQuery, queryString) => {
+exports.checksSortByDesc = (sortByQuery, queryString) => {
 
     if(sortByQuery === "title") {
-        return db.query(queryString += ` ORDER BY title ASC`)
+        return db.query(queryString += ` GROUP BY articles.article_id ORDER BY title DESC`)
     }
 
     if(sortByQuery === "article_id") {
-        return db.query(queryString += ` ORDER BY article_id ASC`)
+        return db.query(queryString += ` GROUP BY articles.article_id ORDER BY article_id DESC`)
     }
 
     if(sortByQuery === "topic") {
-        return db.query(queryString += ` ORDER BY topic ASC`)
+        return db.query(queryString += ` GROUP BY articles.article_id ORDER BY topic DESC`)
     }
 
     if(sortByQuery === "votes") {
-        return db.query(queryString += ` ORDER BY votes DESC`)
+        return db.query(queryString += ` GROUP BY articles.article_id ORDER BY votes DESC`)
     }
 
     if(sortByQuery === "comment_count") {
-        return db.query(queryString += ` ORDER BY comment_count DESC`)
+        return db.query(queryString += ` GROUP BY articles.article_id ORDER BY comment_count DESC`)
     }
 
     if(sortByQuery === "created_at") {
-        return db.query(queryString += ` ORDER BY created_at DESC`)
+        return db.query(queryString += ` GROUP BY articles.article_id ORDER BY created_at DESC`)
     }
 
     if(sortByQuery === "author") {
-        return db.query(queryString += ` ORDER BY author ASC`)
+        return db.query(queryString += ` GROUP BY articles.article_id ORDER BY author DESC`)
     }
+}
+
+exports.checksSortByAsc = (sortByQuery, queryString) => {
+
+    if(sortByQuery === "created_at") {
+        return db.query(queryString += ` GROUP BY articles.article_id ORDER BY created_at ASC`)
+    }
+
+}
+
+exports.checkDB = (filter) => {
+    
+   return db.query(`SELECT * FROM topics WHERE slug = $1`, [filter])
+   .then(({rows})=>{
+       if(rows.length === 0) {
+           return Promise.reject({status:404, msg: "Not found"})
+       } else {
+           return Promise.reject({status:200, msg: "Article does not exist"})
+       }
+   })
+
 }
